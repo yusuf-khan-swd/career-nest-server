fc5f70fedfac5f35541fe7c6e2e91a7ddbae0af8
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-06-04 19:28:12 +0600

    update message

--- index.js
+++ index.js
@@ -426,9 +426,9 @@ async function run() {
 run().catch(console.log);
 
 app.get("/", (req, res) => {
-  res.send("Thrift store server is running");
+  res.send("Career Nest server is running");
 });
 
 app.listen(port, () => {
-  console.log(`Thrift store server is running on port ${port}`);
+  console.log(`Career Nest server is running on port ${port}`);
 });
